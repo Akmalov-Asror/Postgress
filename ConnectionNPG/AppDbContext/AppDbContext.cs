011b24293f22dcfa459faf5b286c8388cfc5830e
A3S4ROBOY <akmalovasror0@gmail.com>
2023-08-10 11:42:10 +0300

    Добавьте файлы проекта.

---
+++ ConnectionNPG/AppDbContext/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConnectionNPG.AppDbContext;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+}
---
+++ ConnectionNPG/Program.cs
@@ -0,0 +1,28 @@
+using ConnectionNPG.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
